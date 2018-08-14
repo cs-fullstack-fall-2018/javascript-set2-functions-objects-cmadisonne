b8ae61800159bfa61a5ef1d8ecf9b32f96cc7bd9
Lotus <cmadisonne@gmail.com>
2018-08-14 11:43:09 -0500

    Attempted

---
+++ JS.js
@@ -0,0 +1,19 @@
+document.write("width" , innerWidth);
+document.write("height= " , innerHeight);
+
+window.location.href;
+
+var printInfo = {
+    Title: document.getElementById("Header").title,
+    PageAddress: document.getElementById("Header"),
+    LastModified: document.lastModified,
+};
+
+function getInfo() {
+    document.write("Title: " , printInfo.Title);
+    document.write("Page Address: ", printInfo.PageAddress);
+    document.write("Last Modified: ", printInfo.LastModified);
+}
+
+
+//Im confused but hey here's a comment
