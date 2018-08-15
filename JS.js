4dee82d3287fdae143d54ea1a17bd2f59a91a810
Kevin-CodeCrew <kevin@code-crew.org>
2018-08-15 18:45:16 -0500

    Graded 08-15-2018 18:45:10

--- JS.js
+++ JS.js
@@ -1,19 +1,23 @@
-document.write("width" , innerWidth);
-document.write("height= " , innerHeight);
-
-window.location.href;
-
-var printInfo = {
-    Title: document.getElementById("Header").title,
-    PageAddress: document.getElementById("Header"),
-    LastModified: document.lastModified,
-};
-
-function getInfo() {
-    document.write("Title: " , printInfo.Title);
-    document.write("Page Address: ", printInfo.PageAddress);
-    document.write("Last Modified: ", printInfo.LastModified);
+function getWtHt() { // KEY: The way it was it dumped immediately on first page not 2nd where needs to be. Added some BR (breaks) to space it out
+    document.write("<br/>width=", innerWidth);
+    document.write("<br/>height= ", innerHeight);
 }
+
+// window.location.href; // KEY: Not sure why this was there
+
+// KEY: Nice to see you trying to use an object for passing the values you wanted.
+// KEY: Needed to change how you got title and page address as also need to get from document/DOM
+var printInfo = {
+    Title: document.title,
+    PageAddress: document.URL,
+    LastModified: document.lastModified
+};
+// KEY: Let's just use your object :-)
+// function getInfo() {
+//     document.write("Title: " , printInfo.Title);
+//     document.write("Page Address: ", printInfo.PageAddress);
+//     document.write("Last Modified: ", printInfo.LastModified);
+// }
 
 
 //Im confused but hey here's a comment
